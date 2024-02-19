3401a59f25c2bb3cb5fabbe6c3f6f763ae9dbc40
willy <willyjen0530@gmail.com>
2024-02-19 15:40:31 +0800

    初始化

---
+++ src/main/java/com/willy/springmall/SpringMallApplication.java
@@ -0,0 +1,13 @@
+package com.willy.springmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/willy/springmall/SpringMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willy.springmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
